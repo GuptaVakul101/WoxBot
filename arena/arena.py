5b1c805d5e250a4cbc2fc5c918e4015f6f99f74e
LavishGulati <lavish00gulati@gmail.com>
2020-11-05 00:29:54 +0530

    Handled life increase/decrease

--- arena/arena.py
+++ arena/arena.py
@@ -12,7 +12,13 @@ from NeuralNetwork import NeuralNetwork
 from time import sleep
 from PIL import Image
 import numpy as np
+import math
 import cv2
+
+cube_dict = {}
+pyramid_dict = {}
+global_cube = []
+global_pyramid = []
 
 ground_vertices = ((-GROUND_X_LENGTH / 2, -ROBOT_HEIGHT, GROUND_Z_LENGTH / 2),
                    (GROUND_X_LENGTH / 2, -ROBOT_HEIGHT, GROUND_Z_LENGTH / 2),
@@ -35,6 +41,18 @@ def startLife(fsm):
     return random.randint(0,500)
 
 
+def dist(x1, y1, x2, y2):
+    return math.sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+
+def change_life(life, x, z):
+    for (cube_x, cube_z) in global_cube:
+        if dist(x, z, cube_x, cube_z) <= RADIUS:
+            life -= LIFE_FACTOR
+    for (pyr_x, pyr_z) in global_pyramid:
+        if dist(x, z, pyr_x, pyr_z) <= RADIUS:
+            life += LIFE_FACTOR
+    return life
+
 def arena():
     pygame.init()
     display = (800, 600)
@@ -44,15 +62,20 @@ def arena():
     x_move = 0
     y_move = 0
     z_move = 0
+    x = 0
+    z = 0
     direction = 0
     _cameraAngle = 0
 
-    cube_dict = {}
-    pyramid_dict = {}
     for x in range(NUM_CUBES):
-        cube_dict[x] = setCubeVertices()
+        new_vertices, x_value, z_value = setCubeVertices()
+        cube_dict[x] = new_vertices
+        global_cube.append((x_value, z_value))
+
     for x in range(NUM_PYRAMIDS):
-        pyramid_dict[x] = setPyramidVertices()
+        new_vertices, x_value, z_value = setPyramidVertices()
+        pyramid_dict[x] = new_vertices
+        global_pyramid.append((x_value, z_value))
 
     life = INITIAL_LIFE
     for i in range(0, MAX_TIME):
@@ -102,13 +125,17 @@ def arena():
             direction %= 4
 
         if direction == 0:
-            z_move = 1
+            z_move = MOVE_SIZE
+            z += MOVE_SIZE
         elif direction == 1:
-            x_move = 1
+            x_move = MOVE_SIZE
+            x += MOVE_SIZE
         elif direction == 2:
-            z_move = -1
+            z_move = -MOVE_SIZE
+            z -= MOVE_SIZE
         elif direction == 3:
-            x_move = -1
+            x_move = -MOVE_SIZE
+            x -= MOVE_SIZE
 
         glClear((GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT))
         glTranslatef(x_move, y_move, z_move)
@@ -123,6 +150,9 @@ def arena():
             Cubes(cube_dict[cube])
         for pyramid in pyramid_dict:
             Pyramids(pyramid_dict[pyramid])
+
+        life = change_life(life, x, z)
+        print('Current life', life)
 
         pygame.display.flip()
 
